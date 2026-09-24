f45de28114966c49cbbc1b3d4747c30762a7781f
Reeke <rtilley2007@gmail.com>
2026-09-24 10:17:56 -0400

    All Intake commands implemented

--- src/main/java/frc/robot/commands/intake/AutoActivateIntakeCmd.java
+++ src/main/java/frc/robot/commands/intake/AutoActivateIntakeCmd.java
@@ -1,5 +1,31 @@
 package frc.robot.commands.intake;
 
-public class AutoActivateIntakeCmd {
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.subsystems.IntakeSubsys;
 
+public class AutoActivateIntakeCmd extends Command {
+    private final IntakeSubsys INTAKE;
+    boolean finished = false;
+
+    public AutoActivateIntakeCmd(IntakeSubsys intake) {
+        super();
+        this.INTAKE = intake;
+        this.addRequirements(intake); 
+    }
+
+    @Override
+    public void initialize() {
+        INTAKE.startIntake();
+        finished = true;
+    }
+
+    @Override
+    public void execute() {
+
+    }
+
+    @Override
+    public boolean isFinished() {
+        return finished;
+    }
 }
--- src/main/java/frc/robot/commands/intake/AutoIntakeDownCmd.java
+++ src/main/java/frc/robot/commands/intake/AutoIntakeDownCmd.java
@@ -1,5 +1,34 @@
 package frc.robot.commands.intake;
 
-public class AutoIntakeDownCmd {
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.subsystems.IntakeSubsys;
 
+public class AutoIntakeDownCmd extends Command {
+    private final IntakeSubsys INTAKE;
+    boolean finished = false;
+
+
+    public AutoIntakeDownCmd(IntakeSubsys intake) {
+        super();
+        this.INTAKE = intake;
+        this.addRequirements(intake); 
+    }
+
+    @Override
+    public void initialize() {
+        INTAKE.pivotDown();
+    }
+
+    @Override
+    public void execute() {
+        if (INTAKE.isSwitchHit()) {
+            INTAKE.pivotStop();
+            finished = true;
+        }
+    }
+
+    @Override
+    public boolean isFinished() {
+        return finished;
+    }
 }
